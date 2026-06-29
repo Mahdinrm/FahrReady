6de396b8f222c54d503cf7d788db30a82ea4013a
Mahdi NARIMANI <azxabarnews@gmail.com>
2026-06-29 08:50:04 +0200

    Fix nav/status bar overlap with explicit color and no layout flags

--- android/app/src/main/java/com/fahrready/MainActivity.kt
+++ android/app/src/main/java/com/fahrready/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.fahrready
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.core.view.WindowCompat
+import android.view.View
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -10,8 +12,20 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    WindowCompat.setDecorFitsSystemWindows(window, true)
     super.onCreate(savedInstanceState)
+
+    // Force status bar and nav bar to NOT overlap content
+    val w = window
+    w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+    w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    w.statusBarColor = Color.parseColor("#07090F")
+    w.navigationBarColor = Color.parseColor("#07090F")
+
+    // Remove layout flags that cause overlap
+    w.decorView.systemUiVisibility = (
+      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    )
   }
 
   override fun getMainComponentName(): String = "FahrReady"
